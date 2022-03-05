af3b504f133e2287a03861d999a9d7bcc0ec9394
d1snin <~@d1s.uno>
2022-03-05 20:54:31 +0300

    added jupiter engine for `teabag-stdlib`

--- teabag-stdlib/build.gradle.kts
+++ teabag-stdlib/build.gradle.kts
@@ -1,3 +1,9 @@
+val junitVersion: String by project
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+}
+
 publishing {
     publications {
         create<MavenPublication>("teabag-stdlib") {
