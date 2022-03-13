3853611f63de577538653d18dd357d1fa63e7b9c
d1snin <me@d1s.dev>
2022-03-13 15:26:18 +0300

    refactored `ServletUriComponentsBuilderKt`

--- teabag-spring-web/src/main/kotlin/dev/d1s/teabag/web/ServletUriComponentsBuilder.kt
+++ teabag-spring-web/src/main/kotlin/dev/d1s/teabag/web/ServletUriComponentsBuilder.kt
@@ -3,4 +3,9 @@ package dev.d1s.teabag.web
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder
 
 public fun appendPath(value: String): String =
-    ServletUriComponentsBuilder.fromCurrentRequest().path(value).build().path!!
+    ServletUriComponentsBuilder
+        .fromCurrentRequest()
+        .path("/{value}")
+        .buildAndExpand(value)
+        .toUri()
+        .toString()
