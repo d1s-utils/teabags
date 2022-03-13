185b33b85c44494cdf7a986ba089eb00f544ca79
d1snin <me@d1s.dev>
2022-03-13 15:06:34 +0300

    added utility for `ServletUriComponentsBuilder`

---
+++ teabag-spring-web/src/main/kotlin/dev/d1s/teabag/web/ServletUriComponentsBuilder.kt
@@ -0,0 +1,6 @@
+package dev.d1s.teabag.web
+
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder
+
+public fun appendPath(value: String): String =
+    ServletUriComponentsBuilder.fromCurrentRequest().path(value).build().path!!
---
+++ teabag-spring-web/src/test/kotlin/dev/d1s/teabag/web/ServletUriComponentsBuilderTest.kt
@@ -0,0 +1,25 @@
+package dev.d1s.teabag.web
+
+import dev.d1s.teabag.testing.constant.VALID_STUB
+import io.mockk.every
+import io.mockk.mockkStatic
+import org.junit.jupiter.api.Test
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder
+import strikt.api.expectThat
+import strikt.assertions.isEqualTo
+
+internal class ServletUriComponentsBuilderTest {
+
+    @Test
+    fun `should return valid path`() {
+        mockkStatic(ServletUriComponentsBuilder::class) {
+            every {
+                ServletUriComponentsBuilder.fromCurrentRequest().path(VALID_STUB).build().path!!
+            } returns VALID_STUB
+
+            expectThat(
+                appendPath(VALID_STUB)
+            ).isEqualTo(VALID_STUB)
+        }
+    }
+}
