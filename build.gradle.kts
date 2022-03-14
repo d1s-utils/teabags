9f8230934fc49d841af673a0e1857bc884597de8
d1snin <me@d1s.dev>
2022-03-14 17:29:24 +0300

    bumped to `2.6.1-stable.0`

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ allprojects {
     }
 
     group = "dev.d1s"
-    version = "2.6.0-stable.0"
+    version = "2.6.1-stable.0"
     java.sourceCompatibility = JavaVersion.VERSION_11
 
     repositories {
