d28904f426c9ad35feeb10c84440ad23b618d703
d1snin <me@d1s.dev>
2022-03-20 22:46:58 +0300

    added utility for `Iterable`

--- teabag-stdlib/src/main/kotlin/dev/d1s/teabag/stdlib/collection/IterableExt.kt
+++ teabag-stdlib/src/main/kotlin/dev/d1s/teabag/stdlib/collection/IterableExt.kt
@@ -1,5 +1,7 @@
 package dev.d1s.teabag.stdlib.collection
 
+import kotlin.properties.Delegates
+
 public infix fun <T> Iterable<T>.anyIn(other: Iterable<T>): Boolean {
     forEach {
         if (it in other) {
@@ -8,4 +10,14 @@ public infix fun <T> Iterable<T>.anyIn(other: Iterable<T>): Boolean {
     }
 
     return false
+}
+
+public inline fun <T, R : Any> Iterable<T>.multiply(block: (T) -> R): R {
+    var result: R by Delegates.notNull()
+
+    forEach {
+        result = block(it)
+    }
+
+    return result
 }
