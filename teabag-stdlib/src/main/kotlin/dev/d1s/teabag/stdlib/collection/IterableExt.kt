105d86aadb091dd8c1401f981d027365f8b9ccd4
d1snin <me@d1s.dev>
2022-03-20 23:11:54 +0300

    tested and fixed the utility for `Iterable`

--- teabag-stdlib/src/main/kotlin/dev/d1s/teabag/stdlib/collection/IterableExt.kt
+++ teabag-stdlib/src/main/kotlin/dev/d1s/teabag/stdlib/collection/IterableExt.kt
@@ -1,7 +1,5 @@
 package dev.d1s.teabag.stdlib.collection
 
-import kotlin.properties.Delegates
-
 public infix fun <T> Iterable<T>.anyIn(other: Iterable<T>): Boolean {
     forEach {
         if (it in other) {
@@ -12,12 +10,12 @@ public infix fun <T> Iterable<T>.anyIn(other: Iterable<T>): Boolean {
     return false
 }
 
-public inline fun <T, R : Any> Iterable<T>.multiply(block: (T) -> R): R {
-    var result: R by Delegates.notNull()
+public inline fun <T, P : Any> Iterable<T>.multiply(principalObj: P, block: (T, P) -> P): P {
+    var principal = principalObj
 
     forEach {
-        result = block(it)
+        principal = block(it, principal)
     }
 
-    return result
+    return principal
 }
--- teabag-stdlib/src/test/kotlin/dev/d1s/teabag/stdlib/collection/IterableExtTest.kt
+++ teabag-stdlib/src/test/kotlin/dev/d1s/teabag/stdlib/collection/IterableExtTest.kt
@@ -15,4 +15,15 @@ internal class IterableExtTest {
     fun `should return false`() {
         expectThat(setOf(1, 2, 3) anyIn setOf(4, 5, 6)).isEqualTo(false)
     }
+
+    @Test
+    fun `should iterate over the collection and return the value calculated in the block`() {
+        val string = "abc"
+
+        expectThat(
+            listOf("a", "b", "c").multiply(string) { it, principal ->
+                principal.replace(it, "r")
+            }
+        ).isEqualTo("rrr")
+    }
 }
