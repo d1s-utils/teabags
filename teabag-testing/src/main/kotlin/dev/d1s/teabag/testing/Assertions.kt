8047cfdd6efd4ec0bcb6eea09f5f3d271f6f1316
d1snin <me@d1s.dev>
2022-03-30 22:36:31 +0300

    added `assertThrowsBlocking` to `Assertions.kt`

--- teabag-testing/src/main/kotlin/dev/d1s/teabag/testing/Assertions.kt
+++ teabag-testing/src/main/kotlin/dev/d1s/teabag/testing/Assertions.kt
@@ -2,9 +2,16 @@ package dev.d1s.teabag.testing
 
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
 
 public inline fun <R> assertDoesNotThrowBlocking(crossinline block: suspend () -> R): R = runBlocking {
     assertDoesNotThrow {
+        block()
+    }
+}
+
+public inline fun <reified E : Exception> assertThrowsBlocking(crossinline block: suspend () -> Unit): E = runBlocking {
+    assertThrows {
         block()
     }
 }
