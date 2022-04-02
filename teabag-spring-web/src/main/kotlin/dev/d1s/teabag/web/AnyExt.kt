cc03532324404257a742ebbfb9abf868718f92a8
d1snin <me@d1s.dev>
2022-04-02 23:04:44 +0300

    removed AnyExt.kt

--- teabag-spring-web/src/main/kotlin/dev/d1s/teabag/web/AnyExt.kt
+++
@@ -1,5 +0,0 @@
-package dev.d1s.teabag.web
-
-import org.springframework.http.ResponseEntity
-
-public fun <T> ok(body: T): ResponseEntity<Any> = ResponseEntity.ok(body)
--- teabag-spring-web/src/main/kotlin/dev/d1s/teabag/web/ResponseEntity.kt
+++ teabag-spring-web/src/main/kotlin/dev/d1s/teabag/web/ResponseEntity.kt
@@ -3,4 +3,3 @@ package dev.d1s.teabag.web
 import org.springframework.http.ResponseEntity
 
 public val noContent: ResponseEntity<Any> = ResponseEntity.noContent().build()
-
