94c4da9ceced6fc8014f5c7cfe4166207b51ba36
d1snin <me@d1s.dev>
2022-04-11 17:38:03 +0300

    bumped to `5.2.1-stable.1`

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ allprojects {
     }
 
     group = "dev.d1s"
-    version = "5.2.1-stable.0"
+    version = "5.2.1-stable.1"
     java.sourceCompatibility = JavaVersion.VERSION_11
 
     repositories {
