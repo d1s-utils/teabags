dc807bcb95dc4aaa5fc8f016ba346222324edd86
d1snin <me@d1s.dev>
2022-04-14 23:01:07 +0300

    made producing map's values of `mapOfNotNullValues` not nullable

--- teabag-stdlib/src/main/kotlin/dev/d1s/teabag/stdlib/collection/Maps.kt
+++ teabag-stdlib/src/main/kotlin/dev/d1s/teabag/stdlib/collection/Maps.kt
@@ -2,4 +2,6 @@ package dev.d1s.teabag.stdlib.collection
 
 public fun <K : Any, V> mapOfNotNullValues(vararg pairs: Pair<K, V>): Map<K, V> = pairs.filter {
     it.second != null
-}.toMap()
+}.associate {
+    it.first to it.second!!
+}
