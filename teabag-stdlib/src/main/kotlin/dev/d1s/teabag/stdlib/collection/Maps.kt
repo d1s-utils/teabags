cd227b5f3f453d9d0f54fd8aa4bbea676f687a42
d1snin <me@d1s.dev>
2022-04-15 16:45:56 +0300

    removed Maps.kt

--- teabag-stdlib/src/main/kotlin/dev/d1s/teabag/stdlib/collection/Maps.kt
+++
@@ -1,7 +0,0 @@
-package dev.d1s.teabag.stdlib.collection
-
-public fun <K : Any, V> mapOfNotNullValues(vararg pairs: Pair<K, V>): Map<K, V & Any> = pairs.filter {
-    it.second != null
-}.associate {
-    it.first to it.second!!
-}
