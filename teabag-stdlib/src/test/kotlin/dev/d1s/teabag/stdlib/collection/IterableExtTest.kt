370ceb810fdf4e03d1d4403dc9e92f3782911293
d1snin <me@d1s.dev>
2022-04-29 19:39:07 +0300

    improved tests

--- teabag-stdlib/src/test/kotlin/dev/d1s/teabag/stdlib/collection/IterableExtTest.kt
+++ teabag-stdlib/src/test/kotlin/dev/d1s/teabag/stdlib/collection/IterableExtTest.kt
@@ -2,6 +2,7 @@ package dev.d1s.teabag.stdlib.collection
 
 import org.junit.jupiter.api.Test
 import strikt.api.expectThat
+import strikt.assertions.containsExactly
 import strikt.assertions.isEqualTo
 
 internal class IterableExtTest {
@@ -35,5 +36,27 @@ internal class IterableExtTest {
     @Test
     fun `should return lengthiest line`() {
         expectThat(listOf("-", "--").lengthiestLine()) isEqualTo "--"
+    }
+
+    @Test
+    fun `should return set with transformed values`() {
+        expectThat(
+            listOf(
+                1, 2, 3
+            ).mapToSet {
+                it.toString()
+            }
+        ).containsExactly("1", "2", "3")
+    }
+
+    @Test
+    fun `should return mutable set with transformed values`() {
+        expectThat(
+            listOf(
+                1, 2, 3
+            ).mapToMutableSet {
+                it.toString()
+            }
+        ).containsExactly("1", "2", "3")
     }
 }
