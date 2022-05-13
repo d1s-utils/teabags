e99cc3992f690794a11745d951e7c0561c8833a9
d1snin <me@d1s.dev>
2022-05-13 17:43:25 +0300

    added test

--- teabag-dto/src/test/kotlin/dev/d1s/teabag/dto/util/DtoSetConverterFacadeExtTest.kt
+++ teabag-dto/src/test/kotlin/dev/d1s/teabag/dto/util/DtoSetConverterFacadeExtTest.kt
@@ -1,36 +1,34 @@
 package dev.d1s.teabag.dto.util
 
-import dev.d1s.teabag.dto.DtoSetConverterFacade
 import dev.d1s.teabag.dto.testUtil.mockDtoConverter
 import dev.d1s.teabag.testing.constant.VALID_STUB
 import org.junit.jupiter.api.Test
 import strikt.api.expectThat
-import strikt.assertions.isA
 import strikt.assertions.isEqualTo
 import strikt.assertions.isNull
 
-internal class DtoConverterExtTest {
+internal class DtoSetConverterFacadeExtTest {
 
-    private val converter = mockDtoConverter()
-
-    @Test
-    fun `should return valid DtoSetConverterFacade`() {
-        expectThat(
-            converter.converterForSet()
-        ).isA<DtoSetConverterFacade<String, String>>()
-    }
+    private val dtoSetConverterFacade =
+        mockDtoConverter().converterForSet()
 
     @Test
     fun `should conditionally convert to dto and return valid one`() {
         expectThat(
-            converter.convertToDtoIf(VALID_STUB, true)
-        ) isEqualTo VALID_STUB
+            dtoSetConverterFacade.convertToDtoSetIf(
+                setOf(VALID_STUB),
+                true
+            )
+        ) isEqualTo setOf(VALID_STUB)
     }
 
     @Test
     fun `should conditionally convert to dto and return null`() {
         expectThat(
-            converter.convertToDtoIf(VALID_STUB, false)
+            dtoSetConverterFacade.convertToDtoSetIf(
+                setOf(VALID_STUB),
+                false
+            )
         ).isNull()
     }
 }
