b3d00d1524f41afc563bb8433807c0451250553d
d1snin <me@d1s.dev>
2022-06-09 20:43:20 +0300

    made jitpack work

--- teabag-slf4j/build.gradle.kts
+++ teabag-slf4j/build.gradle.kts
@@ -1,9 +1,7 @@
 val slf4jVersion: String by project
-val junitVersion: String by project
 
 dependencies {
     api("org.slf4j:slf4j-api:$slf4jVersion")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
 }
 
 publishing {
