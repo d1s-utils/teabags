736d6684fe055e27059eaa2a475dce262d7633a2
d1snin <me@d1s.dev>
2022-06-09 20:46:38 +0300

    babe please

--- teabag-log4j/build.gradle.kts
+++ teabag-log4j/build.gradle.kts
@@ -1,9 +1,7 @@
 val log4jVersion: String by project
-val junitVersion: String by project
 
 dependencies {
     api("org.apache.logging.log4j:log4j-api:$log4jVersion")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
 }
 
 publishing {
